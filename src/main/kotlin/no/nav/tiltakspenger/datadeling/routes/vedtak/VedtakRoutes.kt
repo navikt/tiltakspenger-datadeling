a8decfdda93bbfde894e6eae5b02d0846a1c5603
tiafiring <tia.firing@nav.no>
2025-09-29 14:53:47 +0200

    1403 gir OBO tilgang til å hente vedtaksinformasjon fra oversikten (#211)

--- src/main/kotlin/no/nav/tiltakspenger/datadeling/routes/vedtak/VedtakRoutes.kt
+++ src/main/kotlin/no/nav/tiltakspenger/datadeling/routes/vedtak/VedtakRoutes.kt
@@ -26,7 +26,7 @@ fun Route.vedtakRoutes(
 ) {
     val logger = KotlinLogging.logger {}
 
-    // Brukes av saas-proxy og arena
+    // Brukes av veilarbportefolje (OBO), saas-proxy og arena
     post("/vedtak/detaljer") {
         logger.debug { "Mottatt POST kall på /vedtak/detaljer - hent vedtaksdetaljer for fnr og periode" }
         val systembruker = call.systembruker(getSystemBrukerMapper()) as? Systembruker ?: return@post
