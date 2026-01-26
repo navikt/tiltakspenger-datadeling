bad3bfd8f6aaabce9e942722d019c358c8b54fc5
Henrik Solum <henrik.solum@nav.no>
2026-01-26 09:19:00 +0100

    1765 Ferie er nullable

--- src/main/kotlin/no/nav/tiltakspenger/datadeling/meldekort/datadeling/routes/ArenaMeldekortResponse.kt
+++ src/main/kotlin/no/nav/tiltakspenger/datadeling/meldekort/datadeling/routes/ArenaMeldekortResponse.kt
@@ -8,7 +8,7 @@ data class ArenaMeldekortResponse(
     val mottatt: LocalDate?,
     val arbeidet: Boolean,
     val kurs: Boolean,
-    val ferie: Boolean,
+    val ferie: Boolean?,
     val syk: Boolean,
     val annetFravaer: Boolean,
     val registrert: LocalDateTime,
