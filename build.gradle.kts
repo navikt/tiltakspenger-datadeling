2b66f115d08c905852cfb455d7305c64f7995c08
John Andre Hestad <john.andre.hestad@nav.no>
2026-07-26 22:27:05 +0200

    Legg til .editorconfig som andre backendrepos

--- build.gradle.kts
+++ build.gradle.kts
@@ -116,6 +116,7 @@ spotless {
             .editorConfigOverride(
                 mapOf(
                     "ktlint_standard_max-line-length" to "off",
+                    "ktlint_standard_no-unused-imports" to "enabled",
                     "ktlint_standard_function-signature" to "disabled",
                     "ktlint_standard_function-expression-body" to "disabled",
                 ),
