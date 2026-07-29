da75a80041e12103e31e5449130fa49d5107090e
John Andre Hestad <john.andre.hestad@nav.no>
2026-07-29 16:25:34 +0200

    Bump felleslibVersion

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import java.io.StringWriter
 
 val ktorVersjon = "3.4.3"
 val testContainersVersion = "2.0.5"
-val felleslibVersion = "0.0.20260727184308"
+val felleslibVersion = "0.0.20260727190913"
 
 buildscript {
     repositories { mavenCentral() }
